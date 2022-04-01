9ebb554995aa93bfc2b9b75fff1f1d26162c1a95
EbulfezSadigov <7ag0he8@code.edu.az>
2022-04-01 07:25:14 +0400

    Static

---
+++ Static/Models/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static.Models
+{
+    static class Calculator
+    {
+        public static int Sum(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+        public static int Subtract(int number1,int number2)
+        {
+            return number1 - number2;
+        }
+        public static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+
+        }
+        public static int Divide(int number1, int number2)
+        {
+            return number1 / number2;
+
+        }
+        public static int Power(this int number){
+            return number * number;
+        }
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,19 @@
+﻿using Static.Models;
+using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum is "+Calculator.Sum(20,30)); 
+            Console.WriteLine("Subtract is "+Calculator.Subtract(20,30)); 
+            Console.WriteLine("Multiply is "+Calculator.Multiply(20,30)); 
+            Console.WriteLine("Divide is "+Calculator.Divide(60,30));
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Power is "+Calculator.Power(number)); 
+        }
+    }
+}
